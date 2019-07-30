542978abe4514c09d3eba26b17870298d6b7e378
Shubham Kandwal <36587738+shubham0297@users.noreply.github.com>
2019-07-30 16:58:17 +0530

    Add files via upload

---
+++ reduxexample/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greeter from './Greeter/Greeter.react';
+
+
+function App() {
+  return (
+             <Greeter></Greeter>
+          )
+  }
+
+export default App;
